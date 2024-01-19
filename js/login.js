8200dc7a5644ebd290f5a2a42cccf25bda625206
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-19 08:47:35 -0600

    update to login.js

---
+++ js/login.js
@@ -0,0 +1,25 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        const response =await fetch('api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({email, password}),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        }else{
+            alert('Failed to login! Please try again');
+        }
+    }
+};
+
+const loginForm = document.querySelector('.login-form');
+if(loginForm) {
+    loginForm.addEventListener('submit', loginFormHandler);
+}
